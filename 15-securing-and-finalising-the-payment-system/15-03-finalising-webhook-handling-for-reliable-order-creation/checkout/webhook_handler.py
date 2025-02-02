c85c49b65267cc80caff047db277e8cde3af745c
AJGreaves <anna.j.greaves@gmail.com>
2025-02-02 20:53:29 +0100

    Adjust code to match video.

--- 15-securing-and-finalising-the-payment-system/15-03-finalising-webhook-handling-for-reliable-order-creation/checkout/webhook_handler.py
+++ 15-securing-and-finalising-the-payment-system/15-03-finalising-webhook-handling-for-reliable-order-creation/checkout/webhook_handler.py
@@ -26,7 +26,7 @@ class StripeWH_Handler:
 
         billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
-        grand_total = round(intent.charges.data[0].amount / 100, 2)
+        grand_total = round(intent.data.charges[0].amount / 100, 2)
 
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
@@ -37,14 +37,14 @@ class StripeWH_Handler:
         try:
             order = Order.objects.get(
                 full_name__iexact=shipping_details.name,
-                email__iexact=billing_details.email,
-                phone_number__iexact=shipping_details.phone,
-                country__iexact=shipping_details.address.country,
-                postcode__iexact=shipping_details.address.postal_code,
-                town_or_city__iexact=shipping_details.address.city,
-                street_address1__iexact=shipping_details.address.line1,
-                street_address2__iexact=shipping_details.address.line2,
-                county__iexact=shipping_details.address.state,
+                    email__iexact=shipping_details.email,
+                    phone_number__iexact=shipping_details.phone,
+                    country__iexact=shipping_details.country,
+                    postcode__iexact=shipping_details.postal_code,
+                    town_or_city__iexact=shipping_details.city,
+                    street_address1__iexact=shipping_details.line1,
+                    street_address2__iexact=shipping_details.line2,
+                    county__iexact=shipping_details.state,
                 grand_total=grand_total,
             )
             order_exists = True
@@ -55,14 +55,14 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
-                    email=billing_details.email,
+                    email=shipping_details.email,
                     phone_number=shipping_details.phone,
-                    country=shipping_details.address.country,
-                    postcode=shipping_details.address.postal_code,
-                    town_or_city=shipping_details.address.city,
-                    street_address1=shipping_details.address.line1,
-                    street_address2=shipping_details.address.line2,
-                    county=shipping_details.address.state,
+                    country=shipping_details.country,
+                    postcode=shipping_details.postal_code,
+                    town_or_city=shipping_details.city,
+                    street_address1=shipping_details.line1,
+                    street_address2=shipping_details.line2,
+                    county=shipping_details.state,
                     grand_total=grand_total,
                 )
                 for item_id, item_data in json.loads(bag).items():
